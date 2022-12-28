35527f3a8b725e1e2a3d4560a337ac83f95e47b4
nokrolikno <vasya.ermakov.2001@mail.ru>
2022-12-28 19:09:46 +0300

    update reducers

--- sources/industrial_map_reduce_reduce.cpp
+++ sources/industrial_map_reduce_reduce.cpp
@@ -20,34 +20,35 @@ void IndustrialMapReduce::Shuffle() {
     }
     size_t cur_new_key = 0;
     for (size_t mapper_i = 0; mapper_i < num_mappers; ++mapper_i) {
-        for (auto const& elem : post_mapper[mapper_i]) {
+        for (size_t mapper_j = 0; mapper_j < post_mapper[mapper_i].size(); ++mapper_j) {
             bool elem_in_reducers = false;
             for (size_t reducer_i = 0; reducer_i < num_reducers; ++reducer_i) {
-                if (keys[reducer_i].contains(elem)) {
+                if (keys[reducer_i].contains(post_mapper[mapper_i][mapper_j].first)) {
                     for (size_t reducer_j = 0; reducer_j < pred_reducer[reducer_i].size(); ++reducer_j) {
-                        if (pred_reducer[reducer_i][reducer_j].first == elem) {
-                            pred_reducer[reducer_i][reducer_j].second.push_back(post_mapper[mapper_i][elem]);
+                        if (pred_reducer[reducer_i][reducer_j].first == post_mapper[mapper_i][mapper_j].first) {
+                            pred_reducer[reducer_i][reducer_j].second.push_back(post_mapper[mapper_i][mapper_j].second);
                             break;
                         }
                     }
+                    elem_in_reducers = true;
                 }
             }
             if (!elem_in_reducers) {
                 std::pair<std::string, std::vector<std::string>> new_pair;
-                new_pair.first = elem;
+                new_pair.first = post_mapper[mapper_i][mapper_j].first;
                 std::vector<std::string> new_vector;
                 new_pair.second = new_vector;
-                new_vector.push_back(post_mapper[mapper_i][elem]);
+                new_vector.push_back(post_mapper[mapper_i][mapper_j].second);
                 if (pred_reducer[cur_new_key].size() == 0) {
                     pred_reducer[cur_new_key].push_back(new_pair);
                 } else {
                     auto it = pred_reducer[cur_new_key].begin();
-                    while (it != pred_reducer[cur_new_key].end() && elem.compare((*it).first) < 0) {
+                    while (it != pred_reducer[cur_new_key].end() && post_mapper[mapper_i][mapper_j].first.compare((*it).first) < 0) {
                         it++;
                     }
                     pred_reducer[cur_new_key].insert(it, new_pair);
                 }
-                keys[cur_new_key].insert(elem);
+                keys[cur_new_key].insert(post_mapper[mapper_i][mapper_j].first);
                 cur_new_key = (cur_new_key + 1) % num_reducers;
             }
         }
