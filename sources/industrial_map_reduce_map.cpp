bdc08e8b9c4d8fce7fc970b198aa26b1ab984531
howardano <58511197+howardano@users.noreply.github.com>
2022-12-28 19:36:26 +0300

    initial commit

---
+++ sources/industrial_map_reduce_map.cpp
@@ -0,0 +1,26 @@
+#include "industrial_map_reduce.hpp"
+#include <fstream>
+#include <sstream>
+
+namespace IndustrialRise {
+    void IndustrialMapReduce::SetMapper(const IMapper &mapper_) {
+        mapper = mapper_;
+    }
+
+    void IndustrialMapReduce::Map(const size_t file_num) {
+        std::ifstream in(tmp_dir + split_prefix + std::to_string(file_num));
+
+        std::string res = "";
+
+        std::string line = "";
+
+        while(std::getline(in, line)) {
+            std::istringstream iss(line);
+            res += line + " ";
+        }
+
+        std::vector<std::pair<std::string, std::string>> post_mapper_element = mapper(res);
+        post_mapper[file_num] = post_mapper_element;
+    }
+}
+
